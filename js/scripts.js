8e0d200ce806399016e98a641bd44cde3576cbf9
Anna Matviyenko <anna.matviyenko@gmail.com>
2016-07-19 22:15:57 -0700

    add backend functionality to js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 //Business Logic//
 var pingPong = function(randomNumber) {
+	var result = [];
+	var index;
+	for (var index = 1; index <= randomNumber; index += 1) 
 	if (randomNumber % 15 === 0) {
-		return "pingpong";
+		result.push("pingpong");
 	} else if (randomNumber % 3 === 0) {
-		return "ping";
+		result.push("ping");
 	} else if ( randomNumber % 5 === 0) {
-		return "pong";
+		result.push("pong");
 	} else {
-		return false;
+		result.push(index);
 	}
+
+	return result;
 };
 
 
@@ -16,10 +21,17 @@ var pingPong = function(randomNumber) {
 //User Interface Logic//
 $(document).ready(function(event) {
 	$("form#pingpong").submit(function(event) {
-	event.preventDefault();
+	
 	var inputNumber = parseInt($("input#number").val());
 	var result = pingPong(inputNumber);
-	$("#output").text(result);
+
+	result.forEach(function(index) {
+	$(".numberList").append("<li>" + index + "</li>");
+	});
+
+	$("#output").show();
 	
+
+	event.preventDefault();
 	});
 });
