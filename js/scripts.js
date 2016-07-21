950e996b0dc25fcec1041a980904806e859b93fe
Anna Matviyenko <anna.matviyenko@gmail.com>
2016-07-20 20:36:12 -0700

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,21 @@
 var pingPong = function(randomNumber) {
 	var result = [];
 	
-	for(var index = 1; index <= randomNumber; index += 1) {
+	for(var index = 1; index <= randomNumber; index++) {
 
-	if (randomNumber % 15 === 0) {
+	if (index % 15 === 0) {
 		result.push("pingpong");
-	} else if (randomNumber % 3 === 0) {
+	} else if (index % 3 === 0) {
 		result.push("ping");
-	} else if ( randomNumber % 5 === 0) {
+	} else if ( index % 5 === 0) {
 		result.push("pong");
 	} else {
 		result.push(index);
 
 	}
-
+};
 	return result;
+
 };
 
 
